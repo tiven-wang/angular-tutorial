85530763d375fbfe015971ee94ec9d870357997c
tiven.wang <i.tiven.wang@gmail.com>
2019-06-14 17:02:27 +0800

    ui5 webcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+// UI5 Web Components used
+import '@ui5/webcomponents/dist/Button';
+import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/TabContainer"; 
+import "@ui5/webcomponents/dist/Tab";
+import "@ui5/webcomponents/dist/TabSeparator";
+import "@ui5/webcomponents/dist/Table";
 
 @NgModule({
   declarations: [
@@ -11,6 +18,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     ProductListComponent,
     ProductAlertsComponent
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule
   ],
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import "@ui5/webcomponents/dist/Button";
 
 @Component({
   selector: 'app-product-list',
